e7feb53da663f89bcb4d639b531220d9d0743167
Kate Hamilton <kate.hamilton828@gmail.com>
2022-03-03 23:43:28 +0300

    readme

--- src/app.js
+++ src/app.js
@@ -1,10 +1,8 @@
-import React from 'react';
-import Header from './components/Header';
-import Footer from './components/Footer';
+import './App.css';
 
 function App() {
   return (
-  <div>
+<div>
   {/*Navagation tags*/}
   <header>
     <h1>
@@ -113,6 +111,7 @@ function App() {
   {/*Footer end*/}
 </div>
 
+   
   );
 }
 
